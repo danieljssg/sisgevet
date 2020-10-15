2154da822d21e12b8c9e02061e579d9a665afef8
Daniel Saud <49404522+danieljssg@users.noreply.github.com>
2020-10-14 20:03:46 -0400

    Add files via upload

---
+++ precache-manifest.624d35a4d7b88a9a9ae562fff84d7e0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf76211a20a69cb4d108e73f05112857",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d08c42e4fdd9a05c0134",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ca47ef174894cd2ae347",
+    "url": "./static/css/main.d52112a6.chunk.css"
+  },
+  {
+    "revision": "d08c42e4fdd9a05c0134",
+    "url": "./static/js/2.c7eb2ddf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.c7eb2ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca47ef174894cd2ae347",
+    "url": "./static/js/main.0290f001.chunk.js"
+  },
+  {
+    "revision": "3616939f88a8a3203313",
+    "url": "./static/js/runtime-main.31759a5d.js"
+  }
+]);
